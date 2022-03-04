37c4a293b4d46d605d5f0c74771e682af5d8635e
ACL1993 <lortagustin@gmail.com>
2022-03-04 15:29:57 -0300

    añadimos componentes

--- PortfolioAgustinLort/src/app/app.module.ts
+++ PortfolioAgustinLort/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ComponentesComponent } from './components/componentes/componentes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComponentesComponent
   ],
   imports: [
     BrowserModule
